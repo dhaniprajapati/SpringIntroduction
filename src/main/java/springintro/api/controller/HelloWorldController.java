38aab4f8128bdbdd10e48ba2759e45e99a886e7d
Miracle0312 <prajapatidhani06@gmail.com>
2025-02-21 23:10:14 +0530

    [Dhani] Add. Hello from BridgeLabz using Rest controller

--- src/main/java/springintro/api/controller/HelloWorldController.java
+++ src/main/java/springintro/api/controller/HelloWorldController.java
@@ -3,15 +3,21 @@ package springintro.api.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //this is a REST controller that handles web requests
 @RestController
+@RequestMapping("/api")
 public class HelloWorldController {
     //method to handle GET requests sent to the "/hello" URL
     @GetMapping("/hello")
     public String sayhello() {
         //when someone visits "/hello" this message will be sent as a response
+        return "Hello from BridgeLabz";
+    }
+    @GetMapping("/message")
+    public String getMessage() {
         return "Hello from BridgeLabz";
     }
 }
